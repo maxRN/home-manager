26ed12d4f3e2ac16f0b9c13d37b9423f4a2c753b
Max Große <max12.gro@gmail.com>
2023-11-23 02:55:58 +0100

    updates

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -18,19 +18,27 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+
     bat
     btop
+    colima
     curl
+    docker-client
     fd
     ffmpeg
-    fzf
+    fnm
+    kubectl
+    kubernetes-helm
     neofetch
     neovim
+    opam
     pandoc
     ripgrep
     tmux
     tree
     tree-sitter
+
+    jq
 
     # fonts
     jetbrains-mono
@@ -53,21 +61,11 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
     ".config/tmux/tmux.conf".source = dotfiles/tmux/tmux.conf;
     ".config/git/config".source = dotfiles/git/config;
     ".config/git/config-uni".source = dotfiles/git/config-uni;
     ".config/git/config-work".source = dotfiles/git/config-work;
     ".config/git/ignore".source = dotfiles/git/ignore;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   # You can also manage environment variables but you will have to manually
@@ -82,6 +80,79 @@
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
     EDITOR = "nvim";
+    XDG_CONFIG_HOME="$HOME/.config";
+    XDG_CACHE_HOME="$HOME/.cache";
+    ZSH_COMPDUMP="$XDG_CACHE_HOME/zsh/zshcompdump";
+    SUMO_HOME="/opt/homebrew/opt/sumo/share/sumo";
+    # For Quarkus Projects
+    TESTCONTAINERS_RYUK_DISABLED="true";
+  };
+
+  programs.fzf = {
+      enable = true;
+      enableZshIntegration = true;
+  };
+
+  programs.zsh = {
+      enable = true;
+      syntaxHighlighting = {
+          enable = true;
+      };
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      shellAliases = {
+          hm = "home-manager";
+          skim ="open -a /Applications/Skim.app";
+          vim = "nvim";
+          zshconfig="nvim ~/.zshrc";
+          ohmyzsh = "nvim ~/.oh-my-zsh";
+          co="git checkout";
+          gdp="git diff -p";
+          glp="git log -p";
+          gsp="git diff --staged -p";
+          gbl="git branch --list";
+          gnew="git switch -c";
+          gs="git status";
+          # switch to main, if it errors switch to master;
+          gsm=" git switch main 2> /dev/null; [ $? -gt 0 ] && git switch master";
+          dbu="docker compose up -d --build";
+          fzvim="fzf | xargs nvim";
+          ll="ls -lah";
+          k="kubectl";
+          wirecard="/Applications/Wireshark.app/Contents/MacOS/Wireshark";
+      };
+      oh-my-zsh = {
+          enable = true;
+          plugins = [ "git" "docker" "docker-compose" "kubectl" "helm" ];
+          theme = "robbyrussell";
+      };
+      initExtra = ''
+      bindkey '^ ' autosuggest-accept
+
+      zs() {
+          exec zsh;
+      }
+
+      function inspiration() {
+          fortune | cowsay -f $(node -e "var c='$(cowsay -l | sed "1d" | paste -s -d " " -)'.split(' ');console.log(c[Math.floor(Math.random()*c.length)])") | lolcat --seed 0 --spread 1.0
+      }
+
+      export PATH=$PATH:~/go/bin
+      export PATH="$PATH:$HOME/.dotfiles/bin"
+      export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+
+      export SDKMAN_DIR="$HOME/.sdkman"
+      [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+      export PATH="/Users/maxrn/bin:$PATH"
+      export PATH="/Users/maxrn/.dotfiles/scripts:$PATH"
+      export PATH="/Users/maxrn/.local/bin:$PATH"
+
+      # Add Visual Studio Code (code)
+      export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+      export PATH="/Users/maxrn/Library/Python/3.10/bin:$PATH"
+      export PATH="./node_modules/.bin:$PATH"
+      '';
   };
 
   fonts.fontconfig.enable = true;
